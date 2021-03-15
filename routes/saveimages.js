a530d46b74962993324892c5815967bc566c8e60
Altaf <74228908+Altaf12sees@users.noreply.github.com>
2021-03-15 21:56:50 +0400

    Update saveimages.js

--- routes/saveimages.js
+++ routes/saveimages.js
@@ -2,11 +2,7 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
  
- aws.config.update({
-     secretAccessKey:'Rgs7gsTWF9Ofz6pFhL8OpVZz++/r5tgXRwbytDAb',
-     accessKeyId:'AKIAX46DF5Z4AO362CO2',
-     region:'me-south-1'
- });
+
  
 const s3 = new aws.S3({})
  
